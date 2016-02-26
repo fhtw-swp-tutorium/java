6c833d212b265f3071f007ae03a666c2d964c655
Thomas Eizinger <thomas.eizinger@gmail.com>
2016-02-26 12:34:36 +0100

    add test class for command pattern

---
+++ command-tests/src/main/java/com/github/fhtw/swp/tutorium/command/CommandTest.java
@@ -0,0 +1,11 @@
+package com.github.fhtw.swp.tutorium.command;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "null")
+public class CommandTest {
+
+}
